48741bf0903f08d3799e9cd2070e1a9cf3e65f95
Alexey Gorokhov <bring974@gmail.com>
2023-01-31 17:17:12 +0300

    add comment

--- src/main/java/iFellow/Main.java
+++ src/main/java/iFellow/Main.java
@@ -1,7 +1,7 @@
 package iFellow;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) { //некий комментарий
         System.out.println("Hello world!");
     }
 }
